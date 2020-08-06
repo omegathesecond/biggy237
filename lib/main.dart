b32bcd83cc1abb3bc946db50024ef5f4dcc0de72
Laslie Georges JR <laslie.georgesjr@gmail.com>
2020-08-06 11:25:01 +0100

    style added, .gitignore updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Biggy237',
+      debugShowCheckedModeBanner: false,
       theme: getTheme(),
       onGenerateRoute: RouteGenerator.generateRoute,
     );
--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("This is biggy237"),
+    return Scaffold(
+      body: Center(child: Text("This is biggy237")),
     );
   }
 }
